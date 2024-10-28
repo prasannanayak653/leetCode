ea3206b4206637d901cc184322ab028b7ea4f298
Prasanna Kumar Nayak <161146304+prasannanayak653@users.noreply.github.com>
2024-10-28 13:47:41 +0530

    Time: 4 ms (100%), Space: 43.4 MB (97.35%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
+        if(x<0){//If the value of the Interger is a Negative value it returns false
             return false;
         }
         int rev=0;
         int temp=x;
-        while(temp!=0){
+        while(temp!=0){//repeats the loops until the temp value is 0. 
             int digit=temp%10;
             rev=rev*10+digit;
             temp/=10;
         }
-        return (rev==x);
+        return (rev==x);//It will return a Boolean value by Comparing the  reverse value and Original value
 
         
         
